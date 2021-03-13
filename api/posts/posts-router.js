90aca2637e9b5c6e7f64f761f3ce25682e536c1e
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-03-13 15:02:23 -0800

    getting by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -14,9 +14,28 @@ postRouter.get("/", (req, res) => {
       res
         .status(500)
         .json({ message: "The post with the specified ID does not exist" });
-    }); // THis? It may need to be query but do some digging
+    });
 });
 
+postRouter.get("/:id", (req, res) => {
+  post
+    .findById(req.params.id)
+    .then((thePost) => {
+      if (thePost) {
+        res.status(200).json(thePost);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "The post information could not be retrieved" });
+    });
+});
 module.exports = postRouter;
 
 // implement your posts router here
