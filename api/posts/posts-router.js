b2988979af1a0d2c55d14e0edf5fb1f8c12de339
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-03-13 14:44:23 -0800

    server and index setup

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,13 @@
 // implement your posts router here
+
+//[GET] R of CRUD - Read Records / Returns all the array of the post objects contained in the database--/api/posts -- If there is an error in retreving from database respond with 500 --{ message: "The posts information could not be retrieved" }`
+
+//[GET] R of CRUD - Read Records / returns the post object with the specified id -- /api/posts/:id
+
+//[POST] C of CRUD - Create / Cretes a post using the info sent inside the request boday and returns the newly created post object -/api/posts
+
+//[PUT] U of CRUD - Update records with the specified id using the data from the request body and returns the modified document-/api/posts/:id
+
+//[DELETE]-- D of CRUD - Removes thr post with the specified id and returns the deleded post /api/posts/:id
+
+//[GET]-- R of CRUD -- Read records/ Returns an array of all the comment objects associated witht he post with the specified id.
--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
 // require your posts router and connect it here
+
+//Imports
+const express = require("express");
+//Instance of express
+const server = express();
+
+//Route for info to go through
+const postRouter = require("./posts/posts-router.js");
+
+//Route required
+server.use(express.json()); //This changes everything into readable json
+server.use("/api/posts", postRouter);
+
+//Exporting out
+module.exports = server;
